5601e789f5de9b3dd30018bb1eefe9f8abae4927
Flawden <topchiidv1998@gmail.com>
2024-11-03 10:36:57 +0300

    Были внесены изменения в контроллеры с целью соответствия спецификации

--- src/main/java/ru/skypro/homework/controller/CommentsController.java
+++ src/main/java/ru/skypro/homework/controller/CommentsController.java
@@ -6,7 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.Comment;
+import ru.skypro.homework.dto.Comments;
 import ru.skypro.homework.dto.CreateOrUpdateAd;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
@@ -16,14 +21,16 @@ import ru.skypro.homework.dto.CreateOrUpdateAd;
 public class CommentsController {
 
     @GetMapping(path = "/{id}/comments")
-    public ResponseEntity<HttpStatus> getCommentsAdv(@PathVariable Integer id) {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Comments> getCommentsAdv(@PathVariable Integer id) {
+        Comment[] comments = new Comment[1];
+        comments[0] = new Comment(0, "", "Валера", 10000, 101, "Текст комментария");
+        return new ResponseEntity<>(new Comments(comments.length, comments), HttpStatus.OK);
     }
 
     @PostMapping(path = "/{id}/comments")
     public ResponseEntity<Comment> addCommentAdv(@PathVariable Integer id,
                                                  @RequestBody CreateOrUpdateAd createOrUpdateAd) {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(new Comment(1, "", "Author", 1000, 1000, "Text"), HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{adId}/comments/{commentId}")
