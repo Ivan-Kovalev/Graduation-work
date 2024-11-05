b3b03ff5698f3961a71cc8bf37f1889e210fe03d
Flawden <topchiidv1998@gmail.com>
2024-11-05 17:36:55 +0300

    Был создан мапер для рекламы

--- src/main/java/ru/skypro/homework/model/AdEntity.java
+++ src/main/java/ru/skypro/homework/model/AdEntity.java
@@ -15,12 +15,36 @@ import java.util.List;
 @Entity
 public class AdEntity {
 
+    public AdEntity(Integer pk, String image, Integer price, String title, String description, UserEntity author) {
+        this.pk = pk;
+        this.image = image;
+        this.price = price;
+        this.title = title;
+        this.description = description;
+        this.author = author;
+    }
+
+    public AdEntity(Integer pk, String image, String title, Integer price, UserEntity author) {
+        this.pk = pk;
+        this.image = image;
+        this.title = title;
+        this.price = price;
+        this.author = author;
+    }
+
+    public AdEntity(String title, Integer price, String description) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer pk;
     private String image;
     private Integer price;
     private String title;
+    private String description;
 
     @OneToMany(mappedBy = "ad")
     private List<CommentEntity> comments;
